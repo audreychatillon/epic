519f88ccd4837c6021ab5b9c7d3c1451e5115adf
audreychatillon <audrey.chatillon@gmail.com>
2026-05-21 10:58:55 +0200

    update bining

--- src/EpicSpectra.cxx
+++ src/EpicSpectra.cxx
@@ -165,7 +165,7 @@ EpicSpectra::EpicSpectra() {
       m_raw_h1[his_name]->Draw();
 
       his_name = prefix + "_Tof";
-      m_raw_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  3000, -200, 28800);
+      m_raw_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  3000, -2000, 58000);
       can_name = base + "_Tof";
       m_raw_can[can_name]->cd(a+1);
       gPad->SetLogy();

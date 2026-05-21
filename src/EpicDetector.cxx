97af4da72483fdde1d1f5d4aa3a47bd3374f58d2
audreychatillon <audrey.chatillon@gmail.com>
2026-05-21 18:45:33 +0200

    debugging

--- src/EpicDetector.cxx
+++ src/EpicDetector.cxx
@@ -382,20 +382,20 @@ void EpicDetector::InitializeDataOutputPhysics(
 ////////////////////////////////////////////////////////////////////////////////
 void EpicDetector::BuildPhysicalEvent() {
 
-  cout << "Enter EpicDetector::BuildPhysicalEvent() " << endl;
-  if(m_RawData->GetFCMult()>0 && m_RawData->GetQmaxIndex()>=0){
-    short  imax   = m_RawData->GetQmaxIndex();
-    cout << "imax = " << imax << ", mult = " << m_RawData->GetFCMult() << endl;
-    if(!m_RawData->GetPulserTrig(imax) && imax < m_RawData->GetFCMult()){
-        short  det    = m_RawData->GetDetNbr(imax);
-        short  anode  = m_RawData->GetAnodeNbr(imax); 
-        double tofraw = m_RawData->GetTofRaw(imax);
-        double q1     = m_RawData->GetQ1(imax);
-        double tofcal = 0.;
-        double e      = TofRaw2Ene(det, anode, q1, tofraw, tofcal);
-        m_Physics->SetHit_fFC(det, anode, tofcal, e);
-    }
-  }
+  //cout << "Enter EpicDetector::BuildPhysicalEvent() " << endl;
+  //if(m_RawData->GetFCMult()>0 && m_RawData->GetQmaxIndex()>=0){
+  //  short  imax   = m_RawData->GetQmaxIndex();
+  //  cout << "imax = " << imax << ", mult = " << m_RawData->GetFCMult() << endl;
+  //  if(!m_RawData->GetPulserTrig(imax) && imax < m_RawData->GetFCMult()){
+  //      short  det    = m_RawData->GetDetNbr(imax);
+  //      short  anode  = m_RawData->GetAnodeNbr(imax); 
+  //      double tofraw = m_RawData->GetTofRaw(imax);
+  //      double q1     = m_RawData->GetQ1(imax);
+  //      double tofcal = 0.;
+  //      double e      = TofRaw2Ene(det, anode, q1, tofraw, tofcal);
+  //      m_Physics->SetHit_fFC(det, anode, tofcal, e);
+  //  }
+  //}
 
 }
 

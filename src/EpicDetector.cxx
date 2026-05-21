0d28a226abfd8523a9760ff8bdb7e7973a0ffbc9
audreychatillon <audrey.chatillon@gmail.com>
2026-05-21 18:09:29 +0200

    debugging BuildPhysicalEvent

--- src/EpicDetector.cxx
+++ src/EpicDetector.cxx
@@ -385,7 +385,8 @@ void EpicDetector::BuildPhysicalEvent() {
   cout << "Enter EpicDetector::BuildPhysicalEvent() " << endl;
   if(m_RawData->GetFCMult()>0 && m_RawData->GetQmaxIndex()>=0){
     short  imax   = m_RawData->GetQmaxIndex();
-    if(!m_RawData->GetPulserTrig(imax)){
+    cout << "imax = " << imax << ", mult = " << m_RawData->GetFCMult() << endl;
+    if(!m_RawData->GetPulserTrig(imax) && imax < m_RawData->GetFCMult()){
         short  det    = m_RawData->GetDetNbr(imax);
         short  anode  = m_RawData->GetAnodeNbr(imax); 
         double tofraw = m_RawData->GetTofRaw(imax);

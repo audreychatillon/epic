1a8c04946bd2ea7887f4f80ac7a5ea51c5855050
Audrey Chatillon <audrey.chatillon@gmail.com>
2026-09-18 00:01:44 +0200

    new bining DT HF

--- src/EpicSpectra.cxx
+++ src/EpicSpectra.cxx
@@ -39,10 +39,10 @@ EpicSpectra::EpicSpectra() {
     m_canT0->Divide(1, 2);
 
     m_TimeHF = new TH1F("TimeHF", "TimeHF", 86400, 0, 86400);
-    m_DeltaTimeHF = new TH1F("DT_HF_ifBeamOn", "DT_HF_ifBeamOn", 10000, 2.45, 2.55);
+    m_DeltaTimeHF = new TH1F("DT_HF_ifBeamOn", "DT_HF_ifBeamOn", 10000, 2490, 2510);
 
     m_TimeHF->GetXaxis()->SetTitle("TimeHF [s] 1s/bin");
-    m_DeltaTimeHF->GetXaxis()->SetTitle("Delta TimeHF [ms] 10ns/bin");
+    m_DeltaTimeHF->GetXaxis()->SetTitle("Delta TimeHF [us] 1ns/bin");
 
     m_canT0->cd(1); m_TimeHF->Draw();
     m_canT0->cd(2); gPad->SetLogy();  m_DeltaTimeHF->Draw();

b6574a46ca2f9990b51ae8d0748a493895495115
Audrey Chatillon <audrey.chatillon@gmail.com>
2026-09-17 04:27:31 +0200

    copy paste error

--- src/EpicSpectra.cxx
+++ src/EpicSpectra.cxx
@@ -376,7 +376,7 @@ EpicSpectra::EpicSpectra() {
             his_name = prefix + "_TofRaw_ifFF";
             m_phy_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  20000, 0, 2000);
             m_phy_h1[his_name]->GetXaxis()->SetTitle("Time [ns] 100ps / bin");
-            can_name = base + "_TofRaw_ifFF";
+            can_name = base + "_TofRaw";
             m_phy_can[can_name]->cd(a+1);
             gPad->SetLogy();
             m_phy_h1[his_name]->Draw();

cb7e3e1e5806abde6abb2d753c1210e801c6f571
Audrey Chatillon <audrey.chatillon@gmail.com>
2026-09-18 00:33:16 +0200

    adjust beam period

--- src/EpicDetector.cxx
+++ src/EpicDetector.cxx
@@ -575,8 +575,8 @@ void EpicDetector::BuildRawEvent(const std::string &daq,
           double TimeFC = (double)timestamp + (double)T_cfd - sampler_before_threshold_ns;
           double tof_raw = TimeFC - m_TimeHF_current;
 //TODO CALIBRATION PARAMETER BEAM_PERIOD_NS
-          double gamma_thr = m_Cal.GetValue("EPIC_" + to_string(det) + "_ANODE_" + to_string(anode) + "_GAMMA_PEAK",0) - 10.;
-          if (tof_raw < gamma_thr) tof_raw += 2.4991405 * 1.e+06;
+          double gamma_thr = m_Cal.GetValue("EPIC_" + to_string(det) + "_ANODE_" + to_string(anode) + "_GAMMA_PEAK",0) - 5.;
+          if (tof_raw < gamma_thr) tof_raw += (m_TimeHF_current - m_TimeHF_prev) ;
           if (tof_raw < m_TofRaw_max[index] || m_TofRaw_max[index] < 0) {
             m_RawData->SetDetNbr(det);
             m_RawData->SetAnodeNbr(anode);

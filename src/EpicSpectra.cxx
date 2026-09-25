71c9c7526f29e11bb00b5ac1479267627ecfdfe6
audreychatillon <audrey.chatillon@gmail.com>
2026-09-25 18:13:43 +0200

    change bining

--- src/EpicSpectra.cxx
+++ src/EpicSpectra.cxx
@@ -414,15 +414,15 @@ EpicSpectra::EpicSpectra() {
             int gp = (int)m_Cal.GetValue("EPIC_"+ to_string(det)+"_ANODE_"+to_string(anode) + "_GAMMA_PEAK",0);
 
             his_name = prefix + "_TofRaw";
-            m_phy_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  5200, -10000, 5100000);
-            m_phy_h1[his_name]->GetXaxis()->SetTitle("Time [ns] 1uss / bin");
+            m_phy_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  5000, -1000, 4000);
+            m_phy_h1[his_name]->GetXaxis()->SetTitle("Time [ns] 1ns / bin");
             can_name = base + "_TofRaw";
             m_phy_can[can_name]->cd(a+1);
             gPad->SetLogy();
             m_phy_h1[his_name]->Draw();
 
             his_name = prefix + "_TofRaw_cutF_zoom";
-            m_phy_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  20000, gp-200, gp+1800);
+            m_phy_h1[his_name] = new TH1F(his_name.c_str(), his_name.c_str(),  50000, -1000, 4000);
             m_phy_h1[his_name]->GetXaxis()->SetTitle("Time [ns] 100ps / bin");
             can_name = base + "_TofRaw_zoom";
             m_phy_can[can_name]->cd(a+1);
